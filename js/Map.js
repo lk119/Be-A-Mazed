aa2b3ed140a91ced942d466ed4335e72e1d10c59
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2019-11-11 17:11:59 +0000

    added comments and removed redundant code

--- js/Map.js
+++ js/Map.js
@@ -1,35 +1,39 @@
-const TILE_W = 50;
-const TILE_H = 50;
-const MAP_COLS = 16;
-const MAP_ROWS = 12;
-var levelOne =  [1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-								 1, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 4, 0, 0, 1,
-								 1, 0, 1, 1, 4, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
-								 1, 3, 1, 0, 0, 3, 0, 4, 0, 0, 0, 0, 0, 1, 0, 1,
-								 1, 0, 1, 0, 0, 0, 0, 1, 1, 4, 1, 1, 0, 1, 3, 1,
-								 1, 1, 1, 4, 1, 3, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1,
-								 1, 3, 1, 0, 1, 1, 1, 1, 2, 0, 0, 1, 0, 0, 0, 1,
-								 1, 0, 1, 0, 0, 1, 3, 1, 1, 1, 0, 1, 1, 1, 0, 1,
-								 1, 0, 4, 0, 3, 1, 0, 0, 0, 1, 3, 1, 3, 4, 0, 1,
-								 1, 0, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 1,
-								 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+const TILE_W = 50; 			// tile width
+const TILE_H = 50; 			// tile height
+const MAP_COLS = 16;		// number of columns in the grid
+const MAP_ROWS = 12;		// number of rows in the grid
 
+// level one map
+var levelOne =  [1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		 1, 2, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 4, 0, 0, 1,
+		 1, 0, 1, 1, 4, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+		 1, 3, 1, 0, 0, 3, 0, 4, 0, 0, 0, 0, 0, 1, 0, 1,
+		 1, 0, 1, 0, 0, 0, 0, 1, 1, 4, 1, 1, 0, 1, 3, 1,
+		 1, 1, 1, 4, 1, 3, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1,
+		 1, 3, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1,
+		 1, 0, 1, 0, 0, 1, 3, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+		 1, 0, 4, 0, 3, 1, 0, 0, 0, 1, 3, 1, 3, 4, 0, 1,
+		 1, 0, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 0, 1,
+		 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 1,
+		 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+// level two map
 var levelTwo =  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-								 1, 1, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+		 1, 0, 4, 0, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 1,
+		 1, 3, 1, 0, 1, 4, 1, 1, 0, 1, 1, 0, 0, 3, 0, 1,
+		 1, 1, 1, 0, 1, 0, 0, 1, 0, 3, 1, 3, 0, 0, 3, 1,
+		 1, 0, 0, 0, 0, 0, 0, 1, 4, 1, 1, 1, 1, 1, 1, 1,
+		 1, 0, 1, 1, 1, 1, 4, 1, 0, 0, 0, 3, 0, 0, 3, 1,
+		 1, 0, 1, 3, 0, 0, 0, 1, 1, 1, 1, 1, 4, 1, 0, 1,
+		 1, 0, 1, 1, 1, 4, 1, 1, 0, 3, 0, 0, 0, 1, 0, 1,
+		 1, 0, 1, 1, 3, 0, 0, 4, 0, 0, 0, 3, 0, 1, 0, 1,
+		 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+		 1, 1, 2, 1, 3, 0, 0, 0, 0, 4, 0, 3, 0, 0, 0, 1,
+		 1, 1, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
 var gridMap = [];
 
+//the types of tiles
 const TILE_GRASS = 0;
 const TILE_HEDGE = 1;
 const TILE_PLAYERSTART = 2;
@@ -39,36 +43,26 @@ const TILE_OPEN_GATE = 5;
 const TILE_EXIT_ONE = 6;
 const TILE_EXIT_TWO = 7;
 
-
-
-function returnTileTypeAtColRow(col, row) {
-	if(col >= 0 && col < MAP_COLS &&
-		row >= 0 && row < MAP_ROWS) {
-		 var mapIndexUnderCoord = rowColToArrayIndex(col, row);
-		 return gridMap[mapIndexUnderCoord];
-	} else {
-		return TILE_WALL;
-	}
+//function to create the array index for the other functions
+function xyArrayIndex(x, y) {
+	return x + MAP_COLS * y;
 }
 
-function getTileIndexAtCoord(atX, atY) {
-	var playerMapCol = Math.floor(atX / TILE_W);
-	var playerMapRow = Math.floor(atY / TILE_H);
-	var mapIndexUnderPlayer = rowColToArrayIndex(playerMapCol, playerMapRow);
+// finds the coordinates of the player
+function playerTileIndex(x, y) {
+	var playerX = Math.floor(x / TILE_W);
+	var playerY = Math.floor(y / TILE_H);
+	var tileIndexUnderPlayer = xyArrayIndex(playerX, playerY);
 
-	if(playerMapCol >= 0 && playerMapCol < MAP_COLS &&
-		playerMapRow >= 0 && playerMapRow < MAP_ROWS) {
-		return mapIndexUnderPlayer;
+	if(playerX >= 0 && playerX < MAP_COLS &&
+		playerY >= 0 && playerY < MAP_ROWS) {
+		return tileIndexUnderPlayer;
 	}
-
 	return undefined;
 }
 
-function rowColToArrayIndex(col, row) {
-	return col + MAP_COLS * row;
-}
-
-function tileTypeHasTransparency(checkTileType) {
+//makes the certain tiles transparent
+function tileTransparency(checkTileType) {
 	return (
 			checkTileType == TILE_BISCUIT ||
 			checkTileType == TILE_OPEN_GATE ||
@@ -77,23 +71,27 @@ function tileTypeHasTransparency(checkTileType) {
 			checkTileType == TILE_GATE);
 }
 
+// uses for loop to draw the map grid
+//starts with first row and loops through
+// each column and when it reaches the end of a row
+// it goes to the next row
 function drawMap() {
 
 	var arrayIndex = 0;
 	var drawTileX = 0;
 	var drawTileY = 0;
 
-	for(var eachRow=0;eachRow<MAP_ROWS;eachRow++) {
-		for(var eachCol=0;eachCol<MAP_COLS;eachCol++) {
+	for(var rowIndex = 0; rowIndex < MAP_ROWS; rowIndex++) {
+		for(var columnIndex = 0; columnIndex < MAP_COLS; columnIndex++) {
 
-			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+			var arrayIndex = xyArrayIndex(columnIndex, rowIndex);
 			var tileType = gridMap[arrayIndex];
-			var useImg = mapTiles[tileType];
+			var image = mapTiles[tileType];
 
-			if( tileTypeHasTransparency(tileType) ) {
+			if(tileTransparency(tileType) ) {
 				canvasContext.drawImage(mapTiles[TILE_GRASS],drawTileX,drawTileY);
 			}
-			canvasContext.drawImage(useImg,drawTileX,drawTileY);
+			canvasContext.drawImage(image,drawTileX,drawTileY);
 			drawTileX += TILE_W;
 			arrayIndex++;
 		}
@@ -101,4 +99,4 @@ function drawMap() {
 		drawTileX = 0;
 	}
 
-} 
+}
