05fb692c1a4e4ac8fac78abaaa4aea58f3036fe0
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2019-11-18 23:15:55 +0000

    Update Scoreboard.js

--- client/js/Scoreboard.js
+++ client/js/Scoreboard.js
@@ -1,6 +1,10 @@
 $(function() {
 
   var scores = $("#scores")
+  var chatText = $("#chatText")
+	var send = $("#send")
+	var messages = $("#messages")
+
 
 
   socket.on('score', function(data) {
@@ -8,6 +12,19 @@ $(function() {
     scores.append("<p class='score'>" + data.user + ": " + data.score + "</p>")
   })
 
+  //Emit message
+  	send.click(function(){
+  		socket.emit('new_message', {
+        user: socket.id,
+        message: chatText.val()
+      })
+  	})
+
+  	//Listen on new_message
+  	socket.on("new_message", (data) => {
+  		chatText.val('');
+  		messages.append("<p class='chatText'>" + data.user + ": " + data.message + "</p>")
+  	})
 
 
 });
