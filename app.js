d5e9100e4f1d6ed84f0a890be2571688417cee37
Haikah <39999526+Haikah@users.noreply.github.com>
2019-11-18 23:42:30 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express')
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server started.");
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('conncection', function(socket) {
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	socket.number = "" + Math.floor(10 * Math.random());
+	SOCKET_LIST[socket.id] = socket;
+	console.log('socket connection');
+	
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
+	});
+});
+
+setInterval(function(){
+	var pack = [];
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y
+			number:socket.number
+		});
+    }
+    for (var i in SOCKET_LIST){
+		socket.emit('newPosition',{
+			x:socket.x,
+			y:socket.y
+		});
+	}
+    for(var i in SOCKET_LIST){
+       socket.emit('newPosition', pack);
+    }	   
+	
+},1000/25);
