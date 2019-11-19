f44832ce112b2026c06dd80e185591edb7ede3d8
Haikah <39999526+Haikah@users.noreply.github.com>
2019-11-19 04:48:50 +0000

    Update app.js

--- app.js
+++ app.js
@@ -88,14 +88,4 @@ io.sockets.on('connection', function(socket){
 	
 });
 
-setInterval(function(){
-	var pack = {
-		player:Player.update(),
-		bullet:Bullet.update(),
-	}
-	
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.emit('newPositions',pack);
-	}
-},1000/25);
+
