442f90e67ebb156f1f328de51a5ca3d873d8ac55
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2019-11-20 18:26:54 +0000

    Update Setup.js

--- client/js/Setup.js
+++ client/js/Setup.js
@@ -20,18 +20,20 @@ function showPassword() {
 	}
 }
 
-function hideAllGameElements() {
+function hideAllElementsExceptLogin() {
 document.getElementById("gameCanvas").style.display = "none";
 document.getElementById("counterBox").style.display = "none";
 document.getElementById("sidebar").style.display = "none";
 document.getElementById("gameOver").style.display = "none";
 document.getElementById("start").style.display = "none";
 document.getElementById("instructions").style.display = "none";
+document.getElementById("createAccountBox").style.display = "none";
 }
 
 
 function hideButtonShowGame() {
 	document.getElementById("start").style.display = "none";
+  document.getElementById("createAccountBox").style.display = "none";
 	document.getElementById("gameCanvas").style.display = "block";
 	document.getElementById("counterBox").style.display = "block";
 	document.getElementById("sidebar").style.display = "block";
@@ -41,4 +43,21 @@ function hideCanvasShowGameOver() {
 	document.getElementById("gameOver").style.display = "block";
 	document.getElementById("counterBox").style.display = "none";
 	document.getElementById("gameCanvas").style.display = "none";
+}
+
+function hideBoxesShowPlay() {
+  document.getElementById("instructions").style.display = "block";
+  document.getElementById("start").style.display = "block";
+  document.getElementById("loginBox").style.display = "none";
+  document.getElementById("createAccountBox").style.display = "none";
+}
+
+function hideLoginCreateAccount() {
+  document.getElementById("loginBox").style.display = "none";
+  document.getElementById("createAccountBox").style.display = "block";
+}
+
+function hideCreateAccountLogin() {
+  document.getElementById("loginBox").style.display = "block";
+  document.getElementById("createAccountBox").style.display = "none";
 }
