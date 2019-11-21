e2f13468c16e8d140ecafd07c33652ed0f77252a
Haikah <39999526+Haikah@users.noreply.github.com>
2019-11-21 02:11:03 +0000

    Update app.js

--- mazepro/app.js
+++ mazepro/app.js
@@ -25,20 +25,25 @@ var USERS = {
 }
 
 var isValidPassword = function(data,cb){
-	setTimeout(function(){
-		cb(USERS[data.username] === data.password);
-	},10);
+	db.account.find({username:data.username,password:data.password},function(err,res){
+		if(res.length > 0)
+			cb(true);
+		else
+			cb(false);
+	});
 }
 var isUsernameTaken = function(data,cb){
-	setTimeout(function(){
-		cb(USERS[data.username]);
-	},10);
+	db.account.find({username:data.username},function(err,res){
+		if(res.length > 0)
+			cb(true);
+		else
+			cb(false);
+	});
 }
 var addUser = function(data,cb){
-	setTimeout(function(){
-		USERS[data.username] = data.password;
+	db.account.insert({username:data.username,password:data.password},function(err){
 		cb();
-	},10);
+	});
 }
 
 var io = require('socket.io')(serv,{});
