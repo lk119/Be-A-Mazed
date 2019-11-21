8efaee4429ed7db232e05df295056a86e404d00c
Haikah <39999526+Haikah@users.noreply.github.com>
2019-11-21 03:20:28 +0000

    Update app.js

--- mazepro/app.js
+++ mazepro/app.js
@@ -1,3 +1,19 @@
+var mongojs = require("mongojs");
+var db = mongojs('localhost:27017/myGame', ['account','progress']);
+
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server started.");
+
+var SOCKET_LIST = {};
 
 var Entity = function(){
 	var self = {
